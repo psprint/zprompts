1cc94f5a574002fddbf72beb9df8fb6f8aaef7d7
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-06-09 21:19:21 +0200

    Immunity to setopt nofunctionargzero

--- zprompts.plugin.zsh
+++ zprompts.plugin.zsh
@@ -5,6 +5,11 @@
 # to ~/.zshrc.
 #
 
+# This gives immunity to functionargzero being unset
+# _ will be set to last argument to source builtin
+PLUGIN_UNDERSCORE="$_"
+[ "$0" != "$PLUGIN_UNDERSCORE" ] && 0="$PLUGIN_UNDERSCORE"
+
 REPO_DIR="${0%/*}"
 if [[ "${fpath[(r)$REPO_DIR]}" != $REPO_DIR ]]; then
     fpath+=( "$REPO_DIR" )
